2a7fde2f5b368c2bc087ca97076fff2d5e887c12
idrakseyfullayev <idrak.seyfi87@gmail.com>
2023-05-24 18:10:23 +0400

    proyekyt deployed

---
+++ order/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .views import Order, OrderItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product',]
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'created_at', 'first_name', 'last_name']
+    list_filter = ['status', 'created_at',]
+    search_fields = ['first_name', 'address']
+    inlines = [OrderItemInline]    
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
