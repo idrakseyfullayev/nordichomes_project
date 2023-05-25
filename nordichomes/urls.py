285240ebf7bca923ed756615ddd98af427aa7014
idrakseyfullayev <idrak.seyfi87@gmail.com>
2023-05-25 16:11:22 +0400

    update

--- nordichomes/urls.py
+++ nordichomes/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('order/', include('order.urls')),
     path('admin/', admin.site.urls),   
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
